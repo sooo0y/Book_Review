5edc0e997a339e9adfa68c8a8cfcef792e580643
김소연 <jejen205@naver.com>
2022-08-26 22:44:35 +0900

    Header,List HTML 완료 + Form으로 이동 기능 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
-  return 
+
+  const navigate = useNavigate();
+
+  return (
+  <StHeader>
+  <h1>BOOK REVIEW</h1>
+  <button
+    onClick={() => {
+      navigate("/form");
+    }}
+    > 추가하기 </button>
+  </StHeader>
+  );
 };
 
 export default Header;
+
+const StHeader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 150px;
+  width: 100%;
+  background-color: red;
+`;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,9 +2,65 @@ import React from "react";
 import styled from "styled-components";
 
 const List = () => {
-return (
-  <h1> 💜 최강 1조 파이팅입니다 💜 </h1>
+
+  return (
+    <StList>
+      <Content>
+
+        <Card>
+          {/* <img url=''art="My Image"/> */}
+          <ImageBox>Image</ImageBox>
+          <p> 책 제목 </p>
+          <p> 작성자 </p>
+        </Card>
+
+        <Card>
+          {/* <img url=''art="My Image"/> */}
+          <ImageBox>Image</ImageBox>
+          <p> 책 제목 </p>
+          <p> 작성자 </p>
+        </Card>
+
+        <Card>
+          {/* <img url=''art="My Image"/> */}
+          <ImageBox>Image</ImageBox>
+          <p> 책 제목 </p>
+          <p> 작성자 </p>
+        </Card>
+        
+
+      </Content>
+    </StList>
   );
 };
 
 export default List;
+
+const StList = styled.div`
+  padding-top: 150px;
+  :button {
+    align-items: right;
+  }
+`;
+
+const Content = styled.div`
+  /* display: flex; */
+`;
+
+const Card = styled.div`
+  border: 1px solid black;
+  border-radius: 15px;
+  padding: 30px;
+  margin: 10px;
+  width: 200px;
+  height: 250px;
+`;
+
+const ImageBox = styled.div`
+  text-align: center;
+  margin: auto;
+  border: 1px solid black;
+  width: 150px;
+  height: 150px;
+`;
+
