0102494331281b741db5a9d329220adb4d8814f3
김소연 <jejen205@naver.com>
2022-08-29 16:37:13 +0900

    만능버튼

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const CustomButton = ({ title, onClick }) => {
+  return (
+    <StButton onClick={onClick} title={title}>
+      {title}
+    </StButton>
+  );
+};
+
+const StButton = styled.button`
+  width: 70px;
+  height: 30px;
+  background-color: red;
+`;
+
+export default CustomButton;
