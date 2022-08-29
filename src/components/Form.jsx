07f1cf02603e6b8d2c2d7faeeb8836066626ac2b
김소연 <jejen205@naver.com>
2022-08-29 13:33:19 +0900

    catrgory value 받아오기

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,8 @@ import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { addPost } from "../redux/modules/form";
 
-let number = 4;
+let number = 1;
+
 const Form = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -13,34 +14,37 @@ const Form = () => {
   const initialState = {
     id: 0,
     username: "",
-    // category:"", 
+    category: "",
     title: "",
     writer: "",
     body: "",
   };
-  
+
   const [post, setPost] = useState(initialState);
 
+
+  // event handler
   const onChangeHandler = (event) => {
     const { name, value } = event.target;
-    setPost({...post, [name]: value, id: number});
+    setPost({ ...post, [name]: value, id: number });
   };
-  
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    dispatch(addPost({...post, id: number}));
+    dispatch(addPost({ ...post, id: number }));
     setPost(initialState);
     number++;
     navigate('/')
   };
- 
-  const [imageSrc, setImageSrc] = useState('');
 
-  const imageUpload = (fileBlob) => {  
-         
+
+  // image
+  const [imageSrc, setImageSrc] = useState("");
+
+  const imageUpload = (fileBlob) => {
     const reader = new FileReader();
     reader.readAsDataURL(fileBlob);
-    
+
     return new Promise((resolve) => {
       reader.onload = () => {
         setImageSrc(reader.result);
@@ -49,88 +53,106 @@ const Form = () => {
     });
   };
 
+  // category
+  const options = [
+    { value: "", name: "카테고리 선택" },
+    { value: "소설•시•에세이", name: "소설•시•에세이" },
+    { value: "자기계발", name: "자기계발" },
+    { value: "인문학•역사", name: "인문학•역사" },
+    { value: "경제•경영", name: "경제•경영" },
+    { value: "철학•예술•종교", name: "철학•예술•종교" },
+    { value: "기타", name: "기타" },
+  ];
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div>
         <div>
           <label>작성자</label>
-          <input 
-          type="text"
-          name="username"
-          value={post.username}
-          onChange={onChangeHandler} 
+          <input
+            type="text"
+            name="username"
+            value={post.username}
+            onChange={onChangeHandler}
           />
         </div>
         <div>
           <label>카테고리</label>
-          <select name="category">
-            <option value="">카테고리 선택</option>
-            <option value="소설•시•에세이">소설•시•에세이</option>
-            <option value="자기계발">자기계발</option>
-            <option value="인문학•역사">인문학•역사</option>
-            <option value="경제•경영">경제•경영</option>
-            <option value="자연과학">자연과학</option>
-            <option value="철학•예술•종교">철학•예술•종교</option>
-            <option value="기타">기타</option>
+          <select
+            name="category"
+            value={post.category}
+            onChange={onChangeHandler}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.name}
+              </option>
+            ))}
           </select>
         </div>
         <div>
           <label>작가명</label>
-          <input 
-          type="text"
-          name="writer"
-          value={post.writer}
-          onChange={onChangeHandler} 
+          <input
+            type="text"
+            name="writer"
+            value={post.writer}
+            onChange={onChangeHandler}
           />
         </div>
         <div>
           <label>책 제목</label>
-          <input 
-          type="text"
-          name="title"
-          value={post.title}
-          onChange={onChangeHandler} 
+          <input
+            type="text"
+            name="title"
+            value={post.title}
+            onChange={onChangeHandler}
           />
         </div>
         <div>
           <label>내용</label>
-          <input 
-          type="text"
-          name="body"
-          value={post.body}
-          onChange={onChangeHandler} 
+          <input
+            type="text"
+            name="body"
+            value={post.body}
+            onChange={onChangeHandler}
           />
-        </div>      
+        </div>
+        <div></div>
+        <p>이미지를 업로드해주세요</p>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            imageUpload(e.target.files[0]);
+          }}
+        />
+        <div className="preview">
+          {imageSrc && (
+            <img
+              src={imageSrc}
+              alt="preview-img"
+              width="200px"
+              height="250px"
+            />
+          )}
+        </div>
         <div>
-          </div>
-            <p>이미지를 업로드해주세요</p>
-            <input type="file" accept="image/*" onChange={(e) => {
-        imageUpload(e.target.files[0]); }}/>
-            <div className="preview">
-                    {imageSrc && <img src={imageSrc} alt="preview-img" width="200px" height="250px" />}
-            </div>
+          <br></br>
           <div>
-            <br></br>
-          <div>
-            <button onClick={() => { navigate('/') }}>취소</button> 
+            <button
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              취소
+            </button>
             <button>추가하기</button>
-            <button onClick={() => setPost(initialState) }>다시 작성하기</button>
+            <button onClick={() => setPost(initialState)}>다시 작성하기</button>
           </div>
         </div>
       </div>
     </form>
   );
 };
-// const StContainerForm = styled.div`
-//   background-color: whitesmoke;
-//   width: 100%;
-//   max-width: 1200px;
-//   min-width: 800px;
-//   height: 800px;
-// `;
-// const StButtonForm = styled.button`
-//   border: 1px solid ${({ borderColor }) => borderColor};
-//   cursor: pointer;
-// `;
+
 export default Form;
