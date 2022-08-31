c38328f9f4f9b8e36f79f19826b3306e27b1a1f8
Chaewon Yoon(Jamie) <104494969+jamie7dev@users.noreply.github.com>
2022-08-31 23:19:12 +0900

    custom hook 사용(useTitle)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { __getPosts } from "../redux/modules/form";
+import useTitle from '../hooks/useTitle';
 
 const List = () => {
+  const titleUpdater = useTitle('로딩중🫥')
+  
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [like, setLike] = useState([0]);
 
   //json-server
   const { isLoading, error, posts } = useSelector((state) => state.form);
+  
+  //Custom Hook(useTitle)
+  setTimeout(() => titleUpdater('Home'), 3000);
 
   useEffect(() => {
     dispatch(__getPosts());
@@ -24,19 +31,41 @@ const List = () => {
     return <div>{error.message}</div>;
   }
 
+  // category
+
+  const category = [
+    "전체보기",
+    "집밥",
+    "브런치",
+    "야식",
+    "간식",
+    "분식",
+    "다이어트",
+    "기타",
+  ];
+
   return (
     <StList>
       <ListTop>
+        {/* <Category>
+        {category.map((a) => {
+          return (
+            <span>{a}</span>
+          )
+        })}
+        </Category> */}
+
         <Category>
-          <b>전체보기</b>
-          <span> 집밥</span>
-          <span> 브런치</span>
-          <span> 야식</span>
-          <span> 간식</span>
-          <span> 분식</span>
-          <span> 다이어트</span>
-          <span> 기타</span>
+            <span><b>전체보기</b></span>
+            <span>집밥</span>
+            <span>브런치</span>
+            <span>야식</span>
+            <span>간식</span>
+            <span>분식</span>
+            <span>다이어트</span>
+            <span>기타</span>
         </Category>
+
         <button
           onClick={() => {
             navigate("/form");
@@ -48,17 +77,28 @@ const List = () => {
 
       <Content>
         {posts.map((form) => {
+          like.push(0);
           return (
             <Card
               key={form.id}
               onClick={() => {
-                navigate("/Detail");
-                // navigate(`/detail/${form.id}`) 나중에 이렇게 변경
+                navigate(`/detail/${form.id}`);
               }}
             >
-              <p> {form.category} </p>
-                <h2> {form.title} </h2>
-                <b> {form.username} </b>
+              <b> {form.username} </b>
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                  let copy = [...like];
+                  copy[form.id] = copy[form.id] + 1;
+                  setLike(copy);
+                }}
+              >
+                💛
+              </span>{" "}
+              {like[form.id]}
+              <p> <b>{form.category}</b> </p>
+              <h3> {form.title} </h3>
             </Card>
           );
         })}
@@ -77,7 +117,7 @@ const StList = styled.div`
 
 const ListTop = styled.div`
   position: fixed;
-  top: 100px;
+  top: 120px;
   height: 100px;
   width: 100%;
   line-height: 100px;
@@ -90,7 +130,7 @@ const ListTop = styled.div`
     float: right;
     cursor: pointer;
   }
-  
+
   background-color: white;
 `;
 
@@ -98,27 +138,28 @@ const Category = styled.div`
   text-align: center;
   & span {
     margin: 0 15px;
+    padding: 10px;
+    border-radius: 20px;
     cursor: pointer;
+    background-color: #5f9947;
   }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 50px 100px;
+  gap: 50px 50px;
   margin-left: 100px;
+  width: 120%;
 `;
 
 const Card = styled.div`
+  padding-top: 30px;
   border: 1px solid black;
   border-radius: 15px;
   width: 350px;
-  height: 400px;
+  height: 150px;
   text-align: center;
   cursor: pointer;
   /* background-color: ; */
-`;
-
-const Image = styled.img`
-  height: 200px;
 `;
